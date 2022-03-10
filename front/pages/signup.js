0a3ee6b06f72289ddd2d70be62f554aef6b5c5ab
kod0751 <50727457+kod0751@users.noreply.github.com>
2022-03-10 19:21:09 +0900

    회원가입 페이지 업데이트

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,15 +1,112 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Head from "next/head";
+import { Form, Input, Checkbox, Button } from "antd";
+import styled from "styled-components";
+
 import AppLayout from "../components/AppLayout";
+import useInput from "../hooks/useInput";
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
 
 const signup = () => {
+  const [email, onChangeEmail] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password]
+  );
+
+  const [term, setTerm] = useState("");
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(false);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    console.log(id, nickname, password);
+  }, [password, passwordCheck, term]);
+
   return (
-    <>
+    <AppLayout>
       <Head>
-        <title>Store | 회원가입</title>
+        <title>회원가입 | NodeBird</title>
       </Head>
-      <AppLayout>회원가입 페이지</AppLayout>
-    </>
+      <Form onFinish={onSubmit}>
+        <div>
+          <label htmlFor="use-email">이메일</label>
+          <br />
+          <Input
+            name="user-email"
+            type="email"
+            value={email}
+            required
+            onChange={onChangeEmail}
+          />
+        </div>
+        <div>
+          <label htmlFor="use-nick">닉네임</label>
+          <br />
+          <Input
+            name="user-nick"
+            value={nickname}
+            required
+            onChange={onChangeNickname}
+          />
+        </div>
+        <div>
+          <label htmlFor="use-password">비밀번호</label>
+          <br />
+          <Input
+            name="user-password"
+            type="password"
+            value={password}
+            required
+            onChange={onChangePassword}
+          />
+        </div>
+        <div>
+          <label htmlFor="use-password-check">비밀번호 체크</label>
+          <br />
+          <Input
+            name="user-password-check"
+            type="password"
+            value={passwordCheck}
+            required
+            onChange={onChangePasswordCheck}
+          />
+          {passwordError && (
+            <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+          )}
+        </div>
+        <div>
+          <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+            동의합니다.
+          </Checkbox>
+          {termError && <ErrorMessage>약관에 동의하셔야합니다.</ErrorMessage>}
+        </div>
+        <div style={{ margin: 10 }}>
+          <Button type="primary" htmlType="submit">
+            가입하기
+          </Button>
+        </div>
+      </Form>
+    </AppLayout>
   );
 };
 
